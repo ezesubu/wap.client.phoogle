6fdc6cafad2bc0adce4c12257245b4bbfa5e2575
Ezequiel Suarez <ezesubu@gmail.com>
2018-07-16 14:49:29 -0500

    get phones

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+$(function() {
+  $('#findPhone').on('click', function(){
+    $('#loader').show();
+     let brand = $('#searchInput').val();
+    $.get('http://localhost:8080/phoneAPI/search?', {brand})
+      .done(fnShowPhones)
+      .fail(fnErrorResponse);
+  })
+
+});
+
+function fnShowPhones(data){
+  $('#loader').hide();
+  console.log(data);
+  $.each(data, function (index, phone) {
+    console.log(phone);
+  });
+}
+
+
+function fnErrorResponse(xhr, status, exception){
+  console.log(xhr, status, exception);
+}
