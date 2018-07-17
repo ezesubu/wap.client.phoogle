a638a112edc5e8522b5aefae70b0e6a3855f87fc
Ezequiel Suarez <ezesubu@gmail.com>
2018-07-16 19:34:04 -0500

    cart, view data and clear botton

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,37 @@
 'use strict';
+const APIURL = 'http://localhost:8080/phoneAPI/';
+
 $(function() {
-
-
   $('#findPhone').on('click', function(){
     $('#loader').show();
      let brand = $('#searchInput').val();
-    $.get('http://localhost:8080/phoneAPI/search?', {brand})
+    $.get(APIURL + 'search?', {brand})
       .done(fnShowPhones)
       .fail(fnErrorResponse);
-  })
+  });
+
+  $('#searchInput').disableAutoFill();
+
+  $('#clearImages').on('click', function(){
+    $('.card-deck').html('');
+  });
+
+  $('#navCart').on('click', function(){
+    $('.card-deck').html('');
+  });
 
   $('#searchInput').autocomplete({
-    serviceUrl: 'http://localhost:8080/phoneAPI/search',
+    serviceUrl: APIURL + 'search',
     paramName: 'brand',
     transformResult: function(response) {
       return {
-        suggestions: $.map(response.myData, function(dataItem) {
-          console.log(myData);
-          return { value: dataItem.brand, data: dataItem.deviceName };
+        suggestions: $.map(JSON.parse(response), function(dataItem) {
+          return  { value: dataItem.deviceName, data: dataItem  };
         })
       };
-    }
+    },
     onSelect: function (suggestion) {
-      alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+      fnShowPhones([suggestion.data])
     }
   });
 
@@ -30,26 +39,66 @@ $(function() {
 
 function fnShowPhones(data){
   $('#loader').hide();
-  console.log(data);
   $.each(data, function (index, phone) {
-  // <div class="card" style="width: 18rem;">
-  //     <img class="card-img-top" src="..." alt="Card image cap">
-  //     <div class="card-body">
-  //     <h5 class="card-title">Card title</h5>
-  //   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  //   <a href="#" class="btn btn-primary">Go somewhere</a>
-  //   </div>
-  //   </div>
-   $('#content').append($('<div>', {
+    let card = $('<div>', {
       'class': 'card',
-      'style': 'width: 18rem;',
-      'text': phone.deviceName
-  }));
+      'style': 'width: 18rem;'
+    });
 
+    let cardImage= $('<img>',{
+      'class': 'card-img-top',
+      'src': 'https://picsum.photos/200/?blur'
+    });
+    let cardBody = $('<div>',{
+      'class': 'card-body',
+    });
+    let cardTitle = $('<h5>',{
+      'class': 'card-title',
+      'text': phone.deviceName
+    });
+    let CardUlGruop = $('<ul>',{
+      'class': 'list-group list-group-flush',
+    });
+
+    let cardBrand = $('<li>', {
+      'class': 'list-group-item',
+      'text': phone.brand
+    });
+
+    let cardResolution= $('<li>', {
+      'class': 'list-group-item',
+      'text': phone.resolution
+    });
+
+    let cardTechnologyn= $('<li>', {
+      'class': 'list-group-item',
+      'text': phone.technology
+    });
+    let cardBtns= $('<li>', {
+      'class': 'list-group-item',
+
+    });
+    let cardBottonBuy = $('<a>',{
+      'id'    : phone.deviceName,
+      'class' : 'btn btn-primary',
+      'text' : 'Add to cart'
+    }).on('click', function(){
+      let urlPost = APIURL + 'cart';
+      $.post(urlPost,  { brand: $(this).attr('id')})
+        .done(setCart)
+        .error(fnErrorResponse);
+    });
+    cardBtns.append(cardBottonBuy);
+    CardUlGruop.append(cardBrand,cardResolution, cardTechnologyn );
+    cardBody.append(cardTitle,CardUlGruop);
+    card.append(cardImage,cardBody,cardBtns);
+    $('.card-deck').append(card);
   });
 }
 
-
+function setCart(){
+  console.log(data)
+}
 function fnErrorResponse(xhr, status, exception){
   console.log(xhr, status, exception);
 }
